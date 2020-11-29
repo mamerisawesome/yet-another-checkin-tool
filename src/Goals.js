33eb4701e7f4b8f3912b8e1c2da9f94d53fcb6ad
Pepe Bawagan <syk0saje@gmail.com>
2020-11-29 20:26:41 +0800

    Disable goal week selection

--- src/Goals.js
+++ src/Goals.js
@@ -15,12 +15,12 @@ const Goals = () => {
   } = State.useContainer()
 
   return <div>
-    <div>Week (Monday):</div>
-    <div>
-      <button onClick={backOneWeek}>&lt;</button>
-      <input value={selectedWeek} disabled={true} />
-      <button onClick={forwardOneWeek}>&gt;</button>
-    </div>
+    {/* <div>Week (Monday):</div> */}
+    {/* <div> */}
+    {/*   <button onClick={backOneWeek}>&lt;</button> */}
+    {/*   <input value={selectedWeek} disabled={true} /> */}
+    {/*   <button onClick={forwardOneWeek}>&gt;</button> */}
+    {/* </div> */}
     <div>Base hours:</div>
     <input value={baseHours} onChange={setBaseHours} type="number" min="0" step="1" />
     <div>Input goals:</div>
